32417055c6110f4fa61f75584b8273a32d9c0f96
Alper Gokcehan <mail@alperg.com>
2018-09-20 19:56:56 -0400

    make mistake

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
+      console.log(dbExamples)
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
